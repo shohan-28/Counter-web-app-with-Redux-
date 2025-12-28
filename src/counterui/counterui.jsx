b55617404cdc025ef56b71eb34dd55b48229267c
shohan-28 <shohanshawon551@gmail.com>
2025-12-28 21:42:45 +0600

    Updated some files and fixed bugs

--- src/counterui/counterui.jsx
+++ src/counterui/counterui.jsx
@@ -1,35 +1,59 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { decrement, increment} from '../features/counters/counterslice';
+import { decrement, increment, decrement2, increment2 } from '../features/counters/counterslice';
 
 const CounterUi = () => {
-    const count = useSelector((state) => state.counters.value);
+    const CardOneCount = useSelector((state) => state.counters.cardOne);
+    const CardTwoCount = useSelector((state) => state.counters.cardTwo);
     const total = useSelector((state) => state.counters.total);
     const dispatch = useDispatch();
 
-    const handleIncreament = () =>{
+    const handleIncrement = () => {
         dispatch(increment());
     };
 
-    const handleDecreament = () =>{
+    const handleDecrement = () => {
         dispatch(decrement());
     };
 
-    
+     const handleIncrement2 = () => {
+        dispatch(increment2());
+    };
+
+    const handleDecrement2 = () => {
+        dispatch(decrement2());
+    };
+
+
     return (
         <div className=' container mx-auto space-y-5'>
-            <div className='space-y-2 px-15 py-8 flex flex-col justify-center items-center shadow-md'>
-                <p className='text-black text-2xl'>count:{count}</p>
-                <button
-                onClick={() => handleIncreament()}
-                className='text-lg text-white bg-green-700 px-3 py-2 rounded-md cursor-pointer'>Click to Increase</button>
-            </div>
+
+            {/* CARD ONE */}
 
             <div className='space-y-2 px-15 py-8 flex flex-col justify-center items-center shadow-md'>
-                <p className='text-black text-2xl'>count:{count}</p>
-                <button
-                onClick={() => handleDecreament()}
-                className='text-lg text-white bg-red-700 px-3 py-2 rounded-md cursor-pointer'>Click to Decrease</button>
+                <p className='text-black text-2xl'>count:{CardOneCount}</p>
+                <div className='flex justify-between gap-6'>
+                    <button
+                        onClick={() => handleIncrement()}
+                        className='text-lg text-white bg-green-700 px-3 py-2 rounded-md cursor-pointer'>Click to Increase</button>
+                    <button
+                        onClick={() => handleDecrement()}
+                        className='text-lg text-white bg-red-700 px-3 py-2 rounded-md cursor-pointer'>Click to Decrease</button>
+                </div>
+            </div>
+
+            {/* CARD TWO */}
+            
+            <div className='space-y-2 px-15 py-8 flex flex-col justify-center items-center shadow-md'>
+                <p className='text-black text-2xl'>count:{CardTwoCount}</p>
+                <div className='flex justify-between gap-6'>
+                    <button
+                        onClick={() => handleIncrement2()}
+                        className='text-lg text-white bg-green-700 px-3 py-2 rounded-md cursor-pointer'>Click to Increase</button>
+                    <button
+                        onClick={() => handleDecrement2()}
+                        className='text-lg text-white bg-red-700 px-3 py-2 rounded-md cursor-pointer'>Click to Decrease</button>
+                </div>
             </div>
 
             <div className='flex justify-center items-center px-15 py-8 bg-amber-200 rounded-md'>
--- src/features/counters/counterslice.jsx
+++ src/features/counters/counterslice.jsx
@@ -2,17 +2,23 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const counterSlice = createSlice({
   name: 'counter',
-  initialState: { value: 0 , total: 0},
+  initialState: { total: 0 , cardOne: 0, cardTwo: 0 },
   reducers: {
-    increment: (state) => { state.value += 1;
+    increment: (state) => { state.cardOne += 1;
         state.total += 1;
      },
-    decrement: (state) => { state.value -= 1; state.total -= 1;},
+    decrement: (state) => { state.cardOne -= 1; state.total -= 1;},
+
+    increment2: (state) => { state.cardTwo += 1;
+        state.total += 1;
+     },
+    decrement2: (state) => { state.cardTwo -= 1; state.total -= 1;},
+
     incrementByAmount: (state, action) => { state.value += action.payload;  state.total += action.payload;
     },
     
   },
 });
 
-export const { increment, decrement, incrementByAmount, decrementByAmount } = counterSlice.actions;
+export const { increment, decrement, incrementByAmount, decrementByAmount, decrement2, increment2 } = counterSlice.actions;
 export default counterSlice.reducer;
