229e0377b3b4ea73bc083ce5becd67e2adb1601f
shohan-28 <shohanshawon551@gmail.com>
2026-01-19 23:17:17 +0600

    Updated some files and fixed bugs

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counters/counterslice"
+import postSlice from './../features/posts/postSlice';
 
 const store = configureStore({
     reducer: {
         counters: counterReducer,
+        post: postSlice,
+        
     }
 })
 export default store;
